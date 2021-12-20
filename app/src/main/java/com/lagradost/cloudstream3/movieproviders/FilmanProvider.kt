f80f1c93a0eec33f238bac8e24222adedf9a00b6
doteq <doteeqq@gmail.com>
2021-12-20 16:44:19 +0100

    Add homepage and series support for filman.cc

--- app/src/main/java/com/lagradost/cloudstream3/movieproviders/FilmanProvider.kt
+++ app/src/main/java/com/lagradost/cloudstream3/movieproviders/FilmanProvider.kt
@@ -1,45 +1,111 @@
 package com.lagradost.cloudstream3.movieproviders
 
-import android.util.Base64
-import android.util.Log
 import com.google.gson.Gson
-import com.google.gson.JsonObject
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.extractorApis
-import com.lagradost.cloudstream3.utils.getQualityFromName
 import org.jsoup.Jsoup
+import org.jsoup.select.Elements
 
 class FilmanProvider : MainAPI() {
     override val mainUrl = "https://filman.cc"
     override val name = "filman.cc"
     override val lang = "pl"
+    override val hasMainPage = true
     override val supportedTypes = setOf(
-        TvType.Movie
+        TvType.Movie,
+        TvType.TvSeries
     )
+
+    override fun getMainPage(): HomePageResponse {
+        val response = app.get(mainUrl).text
+        val document = Jsoup.parse(response)
+        val lists = document.select(".item-list,.series-list")
+        val categories = ArrayList<HomePageList>()
+        for (l in lists) {
+            val title = l.parent().select("h3").text()
+            val items = ArrayList<SearchResponse>()
+            for (i in l.select(".poster")) {
+                val name = i.select("a[href]").attr("title")
+                val href = i.select("a[href]").attr("href")
+                val poster = i.select("img[src]").attr("src")
+                val year = i.parent().select(".film_year").text().toIntOrNull()
+                val returnValue = if (l.hasClass("series-list")) TvSeriesSearchResponse(
+                    name,
+                    href,
+                    this.name,
+                    TvType.TvSeries,
+                    poster,
+                    year,
+                    null
+                ) else MovieSearchResponse(
+                    name,
+                    href,
+                    this.name,
+                    TvType.Movie,
+                    poster,
+                    year
+                )
+                items.add(returnValue)
+            }
+            categories.add(HomePageList(title, items))
+        }
+        return HomePageResponse(categories)
+    }
 
     override fun search(query: String): List<SearchResponse> {
         val url = "$mainUrl/wyszukiwarka?phrase=$query"
         val response = app.get(url).text
         val document = Jsoup.parse(response)
-        val items = document.select("#advanced-search > div").get(1).select(".item")
-        if (items.isEmpty()) return ArrayList()
-        val returnValue = ArrayList<SearchResponse>()
-        for (i in items) {
-            val href = i.attr("href")
-            val img = i.selectFirst("> img").attr("src").replace("/thumb/", "/big/")
-            val name = i.selectFirst(".title").text()
-            returnValue.add(MovieSearchResponse(name, href, this.name, TvType.Movie, img, null))
+        val movies = document.select("#advanced-search > div").get(1).select(".item")
+        val series = document.select("#advanced-search > div").get(3).select(".item")
+        if (movies.isEmpty() && series.isEmpty()) return ArrayList()
+        fun getVideos(type: TvType, items: Elements): ArrayList<SearchResponse> {
+            val returnValue = ArrayList<SearchResponse>()
+            for (i in items) {
+                val href = i.attr("href")
+                val img = i.selectFirst("> img").attr("src").replace("/thumb/", "/big/")
+                val name = i.selectFirst(".title").text()
+                if (type === TvType.TvSeries) {
+                    returnValue.add(TvSeriesSearchResponse(name, href, this.name, type, img, null, null))
+                } else {
+                    returnValue.add(MovieSearchResponse(name, href, this.name, type, img, null))
+                }
+            }
+            return returnValue
         }
-        return returnValue
+        return getVideos(TvType.Movie, movies) + getVideos(TvType.TvSeries, series)
     }
 
-    override fun load(url: String): LoadResponse? {
+    override fun load(url: String): LoadResponse {
         val response = app.get(url).text
         val document = Jsoup.parse(response)
-        val title = document.select("span[itemprop=title]").text()
+        var title = document.select("span[itemprop=title]").text()
         val data = document.select("#links").outerHtml()
-        return MovieLoadResponse(title, url, name, TvType.Movie, data)
+        val posterUrl = document.select("#single-poster > img").attr("src")
+        val year = document.select(".info > ul").get(0).select("li").get(1).text().toIntOrNull()
+        val plot = document.select(".description").text()
+        val episodesElements = document.select("#episode-list").select("a[href]")
+        if (episodesElements.isEmpty()) {
+            return MovieLoadResponse(title, url, name, TvType.Movie, data, posterUrl, year, plot)
+        }
+        title = document.select("#item-headline").select("h2").text()
+        val episodes = ArrayList<TvSeriesEpisode>()
+        for (episode in episodesElements) {
+            val e = episode.text()
+            val regex = Regex("""\[s(\d{1,3})e(\d{1,3})]""").find(e)
+            if (regex != null) {
+                val eid = regex.groups
+                episodes.add(TvSeriesEpisode(
+                    e.split("]")[1].trim(),
+                    eid[1]?.value?.toInt(),
+                    eid[2]?.value?.toInt(),
+                    episode.attr("href"),
+                ))
+            }
+        }
+        document.select("#episode-list").select("a[href]")
+        return TvSeriesLoadResponse(title, url, name, TvType.TvSeries, episodes, posterUrl, year, plot)
     }
 
     override fun loadLinks(
@@ -51,8 +117,10 @@ class FilmanProvider : MainAPI() {
         if(data.isEmpty()) {
             return false
         }
-        val document = Jsoup.parse(data)
-        val urls = ArrayList<String>()
+        val document = if (data.startsWith("http"))
+            Jsoup.parse(app.get(data).text).select("#links").first()
+            else Jsoup.parse(data)
+
         val items = document.select(".link-to-video")
         for (i in items) {
             val decoded = base64Decode(i.select("a").attr("data-iframe"))
