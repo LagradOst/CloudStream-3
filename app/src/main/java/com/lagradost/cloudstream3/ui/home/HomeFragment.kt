12639523e04f59ffb5b4f121d217f6c6a5ccf2a2
KillerDogeEmpire <87155550+KillerDogeEmpire@users.noreply.github.com>
2022-04-06 20:02:37 -0700

    Fixed Mirrors and donghua

--- app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lagradost/cloudstream3/ui/home/HomeFragment.kt
@@ -145,8 +145,8 @@ class HomeFragment : Fragment() {
             tvs: MaterialButton?,
             docs: MaterialButton?,
             movies: MaterialButton?,
-            asian: MaterialButton?,
             mirror: MaterialButton?,
+            asian_dramas: MaterialButton?
         ): List<Pair<MaterialButton?, List<TvType>>> {
             return listOf(
                 Pair(anime, listOf(TvType.Anime, TvType.OVA, TvType.AnimeMovie, TvType.Donghua)),
@@ -154,8 +154,8 @@ class HomeFragment : Fragment() {
                 Pair(tvs, listOf(TvType.TvSeries)),
                 Pair(docs, listOf(TvType.Documentary)),
                 Pair(movies, listOf(TvType.Movie, TvType.Torrent)),
-                Pair(asian, listOf(TvType.AsianDrama)),
                 Pair(mirror, listOf(TvType.Mirror)),
+                Pair(asian_dramas, listOf(TvType.AsianDrama)),
             )
         }
 
