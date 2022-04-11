e117434ccaa36abeac124dff6460d64625db9925
Zaw <42999156+ImZaw@users.noreply.github.com>
2022-04-11 17:06:34 +0300

    faselhd provider

---
+++ app/src/main/java/com/lagradost/cloudstream3/movieproviders/FaselHDProvider.kt
@@ -0,0 +1,161 @@
+package com.lagradost.cloudstream3.movieproviders
+
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.ExtractorLink
+import org.jsoup.nodes.Element
+
+class FaselHDProvider : MainAPI() {
+    override val lang = "ar"
+    override var mainUrl = "https://faselhd.io"
+    override var name = "FaselHD"
+    override val usesWebView = false
+    override val hasMainPage = true
+    override val supportedTypes = setOf(TvType.TvSeries, TvType.Movie, TvType.Anime)
+
+    private fun String.getIntFromText(): Int? {
+        return Regex("""\d+""").find(this)?.groupValues?.firstOrNull()?.toIntOrNull()
+    }
+
+    private fun Element.toSearchResponse(): SearchResponse? {
+        val url = select("div.postDiv a")?.attr("href") ?: return null
+        val posterUrl = select("div.postDiv a div img")?.attr("data-src") ?:
+                        select("div.postDiv a div img")?.attr("src")
+        val title = select("div.postDiv a div img")?.attr("alt")
+            ?.replace("فيلم|مترجم|اون لاين|مسلسل|مشاهدة|انمي".toRegex(),"")
+        val quality = select(".quality").first()?.text()?.replace("1080p |-".toRegex(), "")
+        return MovieSearchResponse(
+            title!!,
+            url,
+            this@FaselHDProvider.name,
+            TvType.TvSeries,
+            posterUrl,
+            null,
+            null,
+            quality = getQualityFromString(quality)
+        )
+    }
+
+    override suspend fun getMainPage(): HomePageResponse {
+        // Title, Url
+        val moviesUrl = listOf(
+            "Movies" to "$mainUrl/all-movies/page/"+(0..10).random(),
+            "Series" to "$mainUrl/series/page/"+(0..10).random()
+        )
+        val pages = moviesUrl.apmap {
+            val doc = app.get(it.second).document
+            val list = doc.select("div[id=\"postList\"] div[class=\"col-xl-2 col-lg-2 col-md-3 col-sm-3\"]")
+                .mapNotNull { element ->
+                element.toSearchResponse()
+            }
+            HomePageList(it.first, list)
+        }.sortedBy { it.name }
+        return HomePageResponse(pages)
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val q = query.replace(" ","+")
+        val result = arrayListOf<SearchResponse>()
+        val d = app.get("$mainUrl/?s=$q").document
+            d.select("div[id=\"postList\"] div[class=\"col-xl-2 col-lg-2 col-md-3 col-sm-3\"]")
+                .mapNotNull {
+                result.add(it.toSearchResponse()!!)
+            }
+        return result
+    }
+
+
+    override suspend fun load(url: String): LoadResponse {
+        val doc = app.get(url).document
+        val isMovie = doc.select("div.epAll").isEmpty()
+        val posterUrl = doc.select("div.posterImg img").attr("src")
+            ?.ifEmpty { doc.select("div.seasonDiv.active img").attr("data-src") }
+
+        val year = doc.select("div[id=\"singleList\"] div[class=\"col-xl-6 col-lg-6 col-md-6 col-sm-6\"]").firstOrNull {
+            it.text().contains("سنة|موعد".toRegex())
+        }?.text()?.getIntFromText()
+
+        val title =
+          doc.select("title").text().replace(" - فاصل إعلاني", "")
+            .replace("فيلم|مترجم|اون لاين|مسلسل|مشاهدة|انمي|$year".toRegex(),"")
+        // A bit iffy to parse twice like this, but it'll do.
+        val duration = doc.select("div[id=\"singleList\"] div[class=\"col-xl-6 col-lg-6 col-md-6 col-sm-6\"]").firstOrNull {
+                it.text().contains("مدة|توقيت".toRegex())
+            }?.text()?.getIntFromText()
+
+        val synopsis = doc.select("div.singleDesc p")?.text()
+        return if (isMovie) {
+            newMovieLoadResponse(
+                title,
+                url,
+                TvType.Movie,
+                url
+            ) {
+                this.posterUrl = posterUrl
+                this.year = year
+                this.plot = synopsis
+                this.duration = duration
+            }
+        } else {
+            val episodes = ArrayList<Episode>()
+            doc.select("div.epAll a").map {
+                episodes.add(
+                    Episode(
+                        it.attr("href"),
+                        it.text(),
+                        doc.select("div.seasonDiv.active div.title").text().getIntFromText() ?: 1,
+                        it.text().getIntFromText(),
+                        null,
+                        null,
+                        null
+                    )
+                )
+            }
+            doc.select("div[id=\"seasonList\"] div[class=\"col-xl-2 col-lg-3 col-md-6\"] div.seasonDiv")
+                .not(".active").apmap { it ->
+                val s = app.get("$mainUrl/?p="+it.attr("data-href")).document
+                s.select("div.epAll a").map {
+                    episodes.add(
+                        Episode(
+                            it.attr("href"),
+                            it.text(),
+                            s.select("div.seasonDiv.active div.title").text().getIntFromText(),
+                            it.text().getIntFromText(),
+                            null,
+                            null,
+                            null
+                        )
+                    )
+                }
+            }
+            newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes.distinct().sortedBy { it.episode }) {
+                this.duration = duration
+                this.posterUrl = posterUrl
+                this.year = year
+                this.plot = synopsis
+            }
+        }
+    }
+    override suspend fun loadLinks(
+        data: String,
+        isCasting: Boolean,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ): Boolean {
+        val player = app.get(app.get(data).document.select("iframe[name=\"player_iframe\"]").attr("src")).document
+           player.select("div.quality_change").map { it ->
+                it.select("button.hd_btn").map {
+                    callback.invoke(
+                        ExtractorLink(
+                            this.name,
+                            this.name + " - ${it.text()}",
+                            it.attr("data-url"),
+                            this.mainUrl,
+                            2,
+                             isM3u8 = true
+                        )
+                    )
+                }
+            }.flatten()
+        return true
+    }
+}
