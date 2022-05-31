c4ba1a816c2b9d6deb394fe37c38041f4304896e
Saksham Shekher <95137948+OshekharO@users.noreply.github.com>
2022-05-31 10:26:46 +0530

    Updated Providers

--- app/src/main/java/com/lagradost/cloudstream3/movieproviders/DopeboxProvider.kt
+++ app/src/main/java/com/lagradost/cloudstream3/movieproviders/DopeboxProvider.kt
@@ -1,6 +1,6 @@
 package com.lagradost.cloudstream3.movieproviders
 
 class DopeboxProvider : SflixProvider() {
-    override var mainUrl = "https://dopebox.to"
+    override var mainUrl = "https://dopebox.cc"
     override var name = "Dopebox"
 }
