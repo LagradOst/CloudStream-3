c6fb4af420b30b352a73617ecba31505591454e0
hexated <hexated@gmail.com>
2022-06-21 22:54:56 +0700

    fix search

--- app/src/main/java/com/lagradost/cloudstream3/syncproviders/providers/IndexSubtitleApi.kt
+++ app/src/main/java/com/lagradost/cloudstream3/syncproviders/providers/IndexSubtitleApi.kt
@@ -4,7 +4,6 @@ import android.util.Log
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.subtitles.AbstractSubProvider
 import com.lagradost.cloudstream3.subtitles.AbstractSubtitleEntities
-import com.lagradost.cloudstream3.syncproviders.InAppAuthAPIManager
 import com.lagradost.cloudstream3.utils.SubtitleHelper
 
 class IndexSubtitleApi : AbstractSubProvider {
@@ -15,11 +14,27 @@ class IndexSubtitleApi : AbstractSubProvider {
         const val TAG = "INDEXSUBS"
     }
 
-    private fun Number?.getOrdinal(): String? {
-        if (this == null) {
-            return null
+    private fun fixUrl(url: String): String {
+        if (url.startsWith("http")) {
+            return url
         }
-        return when (this) {
+        if (url.isEmpty()) {
+            return ""
+        }
+
+        val startsWithNoHttp = url.startsWith("//")
+        if (startsWithNoHttp) {
+            return "https:$url"
+        } else {
+            if (url.startsWith('/')) {
+                return host + url
+            }
+            return "$host/$url"
+        }
+    }
+
+    private fun getOrdinal(num: Int?) : String? {
+        return when (num) {
             1 -> "First"
             2 -> "Second"
             3 -> "Third"
@@ -59,26 +74,8 @@ class IndexSubtitleApi : AbstractSubProvider {
         }
     }
 
-    private fun fixUrl(url: String): String {
-        if (url.startsWith("http")) {
-            return url
-        }
-        if (url.isEmpty()) {
-            return ""
-        }
-
-        val startsWithNoHttp = url.startsWith("//")
-        if (startsWithNoHttp) {
-            return "https:$url"
-        } else {
-            if (url.startsWith('/')) {
-                return host + url
-            }
-            return "$host/$url"
-        }
-    }
-
     override suspend fun search(query: AbstractSubtitleEntities.SubtitleSearch): List<AbstractSubtitleEntities.SubtitleEntity> {
+        val imdbId = query.imdb ?: 0
         val lang = query.lang
         val queryLang = SubtitleHelper.fromTwoLettersToLanguage(lang.toString())
         val queryText = query.query
@@ -86,21 +83,25 @@ class IndexSubtitleApi : AbstractSubProvider {
         val seasonNum = query.seasonNumber ?: 0
         val yearNum = query.year ?: 0
 
-        val urlItem = ArrayList<String>()
+        val urlItems = ArrayList<String>()
 
         val document = app.get("$host/?search=$queryText").document
 
         document.select("div.my-3.p-3 div.media").map { block ->
             if (seasonNum > 0) {
-                if (block.selectFirst("a")?.attr("href")
+                val name = block.select("strong.text-primary").text().trim()
+                val season = getOrdinal(seasonNum)
+                if ((block.selectFirst("a")?.attr("href")
                         ?.contains(
-                            "${seasonNum.getOrdinal()}",
+                            "$season",
                             ignoreCase = true
-                        )!! && block.selectFirst("strong")!!
-                        .text().trim().contains(queryText, ignoreCase = true)
+                        )!! || name.contains(
+                        "$season",
+                        ignoreCase = true
+                    )) && name.contains(queryText, ignoreCase = true)
                 ) {
                     block.select("div.media").mapNotNull {
-                        urlItem.add(
+                        urlItems.add(
                             fixUrl(
                                 it.selectFirst("a")!!.attr("href")
                             )
@@ -109,26 +110,53 @@ class IndexSubtitleApi : AbstractSubProvider {
                 }
             } else {
                 if (block.selectFirst("strong")!!.text().trim()
-                        .matches(Regex("(?i)^$queryText\$")) && block.select("span[title=Release]")
-                        .text().trim().contains("$yearNum")
+                        .matches(Regex("(?i)^$queryText\$"))
                 ) {
-                    block.select("div.media").mapNotNull {
-                        urlItem.add(
-                            fixUrl(
+                    if (block.select("span[title=Release]").isNullOrEmpty()) {
+                        block.select("div.media").mapNotNull {
+                            val urlItem = fixUrl(
                                 it.selectFirst("a")!!.attr("href")
                             )
-                        )
+                            val itemDoc = app.get(urlItem).document
+                            val id = imdbUrlToIdNullable(itemDoc.selectFirst("div.d-flex span.badge.badge-primary")?.parent()
+                                    ?.attr("href"))?.toLongOrNull()
+                            val year = itemDoc.selectFirst("div.d-flex span.badge.badge-success")
+                                    ?.ownText()
+                                    ?.trim()?.toIntOrNull()
+                            Log.i(TAG, "id => $id \nyear => $year||$yearNum")
+                            if (imdbId > 0) {
+                                if (id == imdbId) {
+                                    urlItems.add(urlItem)
+                                }
+                            } else {
+                                if (year == yearNum) {
+                                    urlItems.add(urlItem)
+                                }
+                            }
+                        }
+                    } else {
+                        if (block.select("span[title=Release]").text().trim()
+                                .contains("$yearNum")
+                        ) {
+                            block.select("div.media").mapNotNull {
+                                urlItems.add(
+                                    fixUrl(
+                                        it.selectFirst("a")!!.attr("href")
+                                    )
+                                )
+                            }
+                        }
                     }
                 }
             }
         }
-
+        Log.i(TAG, "urlItems => $urlItems")
         val results = mutableListOf<AbstractSubtitleEntities.SubtitleEntity>()
 
-        urlItem.forEach { url ->
+        urlItems.forEach { url ->
             val request = app.get(url)
             if (request.isSuccessful) {
-                val maxItem = 10 // maximal item that can takes for avoiding ddos site
+                val maxItem = 10 // prevents ddos site
                 var item = 0
                 request.document.select("div.my-3.p-3 div.media").apmap { block ->
                     if (block.select("span.d-block span[data-original-title=Language]").text().trim()
@@ -141,8 +169,9 @@ class IndexSubtitleApi : AbstractSubProvider {
                             )
                         ).document.select("div.my-3.p-3 div.media").map {
                             if(epNum > 0) {
+//                                Log.i(TAG, "title tv sub => ${it.selectFirst("strong.d-block.text-primary")?.text()!!}")
                                 if (it.selectFirst("strong.d-block.text-primary")?.text()?.trim()
-                                        ?.contains(Regex("(?i)(Season\\s?0?${seasonNum}Episode\\s?0?$epNum)|(S?0?${seasonNum}x?E?0?$epNum[\\s|.])"))!!
+                                        ?.contains(Regex("(?i)((Season)?\\s?0?${seasonNum}?\\s?(Episode)\\s?0?${epNum}[^0-9])|(?i)((S?0?${seasonNum}?E0?${epNum}[^0-9])|(0?${seasonNum}[a-z]0?${epNum}[^0-9]))"))!!
                                 ) {
                                     results.add(
                                         AbstractSubtitleEntities.SubtitleEntity(
