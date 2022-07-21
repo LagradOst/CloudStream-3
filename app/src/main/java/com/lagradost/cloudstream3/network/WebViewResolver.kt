ece5af5c7f48be75ee4f4e3b43cb47acb86249b7
Arjix <53124886+ArjixWasTaken@users.noreply.github.com>
2022-07-21 23:54:45 +0300

    Added AdvancedWebView

--- app/src/main/java/com/lagradost/cloudstream3/network/WebViewResolver.kt
+++ app/src/main/java/com/lagradost/cloudstream3/network/WebViewResolver.kt
@@ -2,6 +2,7 @@ package com.lagradost.cloudstream3.network
 
 import android.annotation.SuppressLint
 import android.net.http.SslError
+import android.view.View
 import android.webkit.*
 import com.lagradost.cloudstream3.AcraApplication
 import com.lagradost.cloudstream3.USER_AGENT
@@ -14,8 +15,280 @@ import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import org.jsoup.Jsoup
 import java.net.URI
-import java.util.concurrent.TimeUnit
+import kotlin.collections.ArrayList
+
+
+enum class WebViewActions {
+    WAIT_FOR_PAGE_LOAD,
+    WAIT_FOR_X_SECONDS,
+    WAIT_FOR_NETWORK_CALL,
+    WAIT_FOR_NETWORK_IDLE,
+    WAIT_FOR_ELEMENT,
+    WAIT_FOR_ELEMENT_GONE,
+    EXECUTE_JAVASCRIPT,
+    WAIT_FOR_ELEMENT_TO_BE_CLICKABLE,
+    RETURN
+}
+
+data class WebViewAction(val actionType: WebViewActions, val parameter: Any = "", val callback: (AdvancedWebView) -> Unit? = {  })
+
+class AdvancedWebView private constructor(
+    val url: String,
+    val actions: ArrayList<WebViewAction>,
+    val referer: String?,
+    val method: String,
+    val callback: (AdvancedWebView) -> Unit = {  }
+) {
+    val headers = mapOf<String, String>()
+    var webView: WebView? = null
+    val remainingActions: ArrayList<WebViewAction> = actions
+    var currentHTML: String = ""
+    val document by lazy { Jsoup.parse(currentHTML) }
+    val Instance = this
+
+    data class Builder(
+        var url: String = "",
+        var actions: ArrayList<WebViewAction> = arrayListOf(),
+        var referer: String? = null,
+        var method: String = "GET",
+    ) {
+        fun setUrl(url: String) = apply { this.url = url }
+        fun setReferer(referer: String) = apply { this.referer = referer }
+        fun setMethod(method: String) = apply { this.method = method }
+
+        fun addAction(action: WebViewAction) = apply { this.actions.add(action) }
+        fun addAllActions(actions: ArrayList<WebViewAction>) = apply { this.actions.addAll(actions) }
+
+        fun waitForElement(selector: String, cb: (AdvancedWebView) -> Unit = {  }) = apply {
+            addAction(WebViewAction(WebViewActions.WAIT_FOR_ELEMENT, selector, cb))
+        }
+        fun waitForElementGone(selector: String, cb: (AdvancedWebView) -> Unit = {  }) = apply {
+            addAction(WebViewAction(WebViewActions.WAIT_FOR_ELEMENT, selector, cb))
+        }
+        fun waitForSeconds(seconds: Long, cb: (AdvancedWebView) -> Unit = {  }) = apply {
+            addAction(WebViewAction(WebViewActions.WAIT_FOR_X_SECONDS, seconds, cb))
+        }
+        fun waitForPageLoad(cb: (AdvancedWebView) -> Unit = {  }) = apply {
+            addAction(WebViewAction(WebViewActions.WAIT_FOR_PAGE_LOAD, "", cb))
+        }
+        fun executeJavaScript(code: String, cb: (AdvancedWebView) -> Unit = {  }) = apply {
+            addAction(WebViewAction(WebViewActions.EXECUTE_JAVASCRIPT, code, cb))
+        }
+        fun destroyWebView() = apply { addAction(WebViewAction(WebViewActions.RETURN, "")) }
+
+        fun build(callback: (AdvancedWebView) -> Unit = { }) = AdvancedWebView(this.url, this.actions, this.referer, this.method, callback)
+    }
+
+    private var actionExecutionsPaused = false
+
+    private suspend fun tryExecuteAction() {
+        if (actionExecutionsPaused) return
+        main {
+            if (remainingActions.size > 0) {
+                val action = remainingActions[0]
+                when (action.actionType){
+                    WebViewActions.WAIT_FOR_ELEMENT -> {
+                        actionExecutionsPaused = true
+                        webView?.evaluateJavascript("document.querySelector(\"${action.parameter}\")") {
+                            if (it == "{}") {
+                                Instance.run(action.callback)
+                                remainingActions.remove(action)
+                            }
+                            actionExecutionsPaused = false
+                        }
+                    }
+
+                    WebViewActions.WAIT_FOR_ELEMENT_GONE -> {
+                        actionExecutionsPaused = true
+                        webView?.evaluateJavascript("document.querySelector(\"${action.parameter}\") == undefined") {
+                            if (it == "true") {
+                                Instance.run(action.callback)
+                                remainingActions.remove(action)
+                            }
+                            actionExecutionsPaused = false
+                        }
+                    }
+
+                    WebViewActions.WAIT_FOR_X_SECONDS -> {
+                        actionExecutionsPaused = true
+                        println("AdvancedWebView :: Waiting for ${remainingActions[0].parameter} seconds...")
+                        delay(action.parameter as Long * 1000)
+                        println("AdvancedWebView :: Finished waiting!")
+                        Instance.run(action.callback)
+                        remainingActions.remove(action)
+                        actionExecutionsPaused = false
+                    }
+
+                    WebViewActions.EXECUTE_JAVASCRIPT -> {
+                        actionExecutionsPaused = true
+                        println("AdvancedWebView :: Executing javascript from action...")
+                        webView?.evaluateJavascript(action.parameter as String) {
+                            println("JavaScript Execution done! Result: <$it>")
+                            Instance.run(action.callback)
+                            remainingActions.remove(action)
+                            actionExecutionsPaused = false
+                        }
+                    }
+
+                    WebViewActions.RETURN -> {
+                        actionExecutionsPaused = true
+                        destroyWebView()
+                        remainingActions.clear()
+                    }
+
+                    else -> return@main
+                }
+            }
+        }
+    }
+
+    private fun destroyWebView() {
+        main {
+            webView?.stopLoading()
+            webView?.destroy()
+            webView = null
+            println("Destroyed the WebView!")
+        }
+    }
+
+    fun start() {
+        main {
+            try {
+                webView = WebView(
+                    AcraApplication.context
+                        ?: throw RuntimeException("No base context in WebViewResolver")
+                ).apply {
+                    // Bare minimum to bypass captcha
+                    settings.javaScriptEnabled = true
+                    settings.domStorageEnabled = true
+                    settings.userAgentString = USER_AGENT
+                    settings.blockNetworkImage = true
+                }
+                webView!!.visibility = View.VISIBLE
+            } catch (e: Exception) {
+                println("Error: Failed to create an Advanced WebView, reason: <${e.message}>")
+                println(e.toString())
+                destroyWebView()
+                callback(this)
+            }
+
+            try {
+                webView?.webViewClient = object : WebViewClient() {
+                    override fun onPageFinished(view: WebView?, url: String?) {
+                        super.onPageFinished(view, url)
+
+                        if (remainingActions.size > 0 && remainingActions[0].actionType == WebViewActions.WAIT_FOR_PAGE_LOAD) {
+                            println("PAGE FINISHED!")
+                            val action = remainingActions[0]
+                            Instance.run(action.callback)
+                            remainingActions.remove(action)
+                        }
+
+                        runBlocking { tryExecuteAction() }
+                    }
+
+                    override fun onLoadResource(view: WebView?, url: String?) {
+                        super.onLoadResource(view, url)
+                        if (remainingActions.size > 0 && remainingActions[0].actionType == WebViewActions.WAIT_FOR_NETWORK_CALL) {
+                            if (URI(url) == URI(remainingActions[0].parameter as String)) {
+                                val action = remainingActions[0]
+                                Instance.run(action.callback)
+                                remainingActions.remove(action)
+                            }
+                        }
+                        runBlocking { tryExecuteAction() }
+                    }
+
+                    override fun shouldInterceptRequest(
+                        view: WebView,
+                        request: WebResourceRequest
+                    ): WebResourceResponse? = runBlocking {
+                        tryExecuteAction()
+
+                        val webViewUrl = request.url.toString()
+
+//                        if (interceptUrl.containsMatchIn(webViewUrl)) {
+//                            fixedRequest = request.toRequest().also {
+//                                if (requestCallBack(it)) destroyWebView()
+//                            }
+//                            println("Web-view request finished: $webViewUrl")
+//                            destroyWebView()
+//                            return@runBlocking null
+//                        }
+
+                        val blacklistedFiles = listOf(
+                            ".jpg", ".png", ".webp", ".mpg",
+                            ".mpeg", ".jpeg", ".webm", ".mp4",
+                            ".mp3", ".gifv", ".flv", ".asf",
+                            ".mov", ".mng", ".mkv", ".ogg",
+                            ".avi", ".wav", ".woff2", ".woff",
+                            ".ttf", ".css", ".vtt", ".srt",
+                            ".ts", ".gif",
+                            // Warning, this might fuck some future sites, but it's used to make Sflix work.
+                            "wss://"
+                        )
+
+                        return@runBlocking try {
+                            when {
+                                blacklistedFiles.any { URI(webViewUrl).path.contains(it) } || webViewUrl.endsWith(
+                                    "/favicon.ico"
+                                ) -> WebResourceResponse(
+                                    "image/png",
+                                    null,
+                                    null
+                                )
+
+                                else -> return@runBlocking super.shouldInterceptRequest(
+                                    view,
+                                    request
+                                )
+                            }
+                        } catch (e: Exception) {
+                            null
+                        }
+                    }
+
+                    override fun onReceivedSslError(
+                        view: WebView?,
+                        handler: SslErrorHandler?,
+                        error: SslError?
+                    ) {
+                        handler?.proceed() // Ignore ssl issues
+                    }
+                }
+                webView?.loadUrl(url, headers.toMap())
+            } catch (e: Exception){
+
+            }
+
+            fun setCurrentHTML() {
+                webView?.evaluateJavascript("document.documentElement.outerHTML") {
+                    currentHTML = it
+                        .replace("\\u003C", "<")
+                        .replace("\\\"", "\"")
+                        .replace("\\n", "\n")
+                        .replace("\\t", "\t")
+                    currentHTML = currentHTML.substring(1 until currentHTML.length-1)
+                }
+            }
+
+            while (remainingActions.size > 0){
+                setCurrentHTML()
+                delay(300)
+                tryExecuteAction()
+                setCurrentHTML()
+            }
+            try {
+                callback(this)
+            } catch (e: Exception) {}
+            destroyWebView()
+        }
+    }
+}
+
+
 
 /**
  * When used as Interceptor additionalUrls cannot be returned, use WebViewResolver(...).resolveUsingWebView(...)
@@ -113,32 +386,13 @@ class WebViewResolver(val interceptUrl: Regex, val additionalUrls: List<Regex> =
                         // Less data, low chance of causing issues.
                         // blockNetworkImage also does this job but i will keep it for the future.
                         val blacklistedFiles = listOf(
-                            ".jpg",
-                            ".png",
-                            ".webp",
-                            ".mpg",
-                            ".mpeg",
-                            ".jpeg",
-                            ".webm",
-                            ".mp4",
-                            ".mp3",
-                            ".gifv",
-                            ".flv",
-                            ".asf",
-                            ".mov",
-                            ".mng",
-                            ".mkv",
-                            ".ogg",
-                            ".avi",
-                            ".wav",
-                            ".woff2",
-                            ".woff",
-                            ".ttf",
-                            ".css",
-                            ".vtt",
-                            ".srt",
-                            ".ts",
-                            ".gif",
+                            ".jpg", ".png", ".webp", ".mpg",
+                            ".mpeg", ".jpeg", ".webm", ".mp4",
+                            ".mp3", ".gifv", ".flv", ".asf",
+                            ".mov", ".mng", ".mkv", ".ogg",
+                            ".avi", ".wav", ".woff2", ".woff",
+                            ".ttf", ".css", ".vtt", ".srt",
+                            ".ts", ".gif",
                             // Warning, this might fuck some future sites, but it's used to make Sflix work.
                             "wss://"
                         )
